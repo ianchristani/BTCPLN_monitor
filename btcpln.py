7df9c9d88da6ab07875a6e20e7ddddceea25c138
ianchristani <ianchristani@gmail.com>
2021-09-24 22:55:21 -0300

    data treatment from the source done.

--- btcpln.py
+++ btcpln.py
@@ -4,16 +4,14 @@ from urllib.request import urlopen
 ls = []
 
 #getting the data in the source
-def getquote():
-    tmp =[]    
+def getquote():   
     pg =  urlopen('https://www.marketwatch.com/investing/cryptocurrency/btcpln')
     soup = BeautifulSoup (pg,'html.parser')
     quote = soup.find_all('bg-quote')
     btc_quote = str((quote[45].contents[0]))
 
-    print(btc_quote)
-    btc_qutoe = btc_quote.replace (',','.')
-    print(btc_quote)
-
+    #treating the string
+    btc_quote = btc_quote.replace(',','.')
+    btc_quote = float(btc_quote)
 
 getquote()
