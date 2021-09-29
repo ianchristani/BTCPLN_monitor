28b4ee475e7a481392d48ad00f16c510af1fea18
ianchristani <ianchristani@gmail.com>
2021-09-29 18:43:01 -0300

    time added and testing some samples.

--- btcpln.py
+++ btcpln.py
@@ -1,17 +1,31 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import time
 
-ls = []
+hdt = {}
 
 #getting the data in the source
-def getquote():   
-    pg =  urlopen('https://www.marketwatch.com/investing/cryptocurrency/btcpln')
-    soup = BeautifulSoup (pg,'html.parser')
-    quote = soup.find_all('bg-quote')
-    btc_quote = str((quote[45].contents[0]))
+def getquote(hdt):
+    quote_p = ''
+    while True:    
+        pg =  urlopen('https://www.marketwatch.com/investing/cryptocurrency/btcpln')
+        soup = BeautifulSoup (pg,'html.parser')
+        quote = soup.find_all('bg-quote')
+        btc_quote = str((quote[45].contents[0]))
 
-    #treating the string
-    btc_quote = btc_quote.replace(',','.')
-    btc_quote = float(btc_quote)
+        #building the storic data
+        if btc_quote != quote_p:
+            quote_p = btc_quote
 
-getquote()
+            #treating the string
+            btc_quote = btc_quote.replace(',','.')
+            btc_quote = float(btc_quote)
+
+            #adding the time to the quote
+            quote_time = str(time.strftime('%H:%M:%S'))
+            hdt [quote_time] = btc_quote
+            hdt = dict(sorted(hdt.items(), key=lambda item:item[1]))
+
+            print(hdt)
+
+getquote(hdt)
